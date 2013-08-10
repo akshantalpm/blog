c960928a9bb7c3d0908c30855dcc70b5cd70be2b
Pooja Akshantal <pooja.akshantal@gmail.com>
2013-08-10 20:52:40 +0530

    created form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e839495ef254ef9cdc08e5ed868319a37e148834fa1a18d22ad53081520e23cd474e46b185c7718f811644f7d95c678f9ff8fc9159fff91c160ca54d9a68308a'
